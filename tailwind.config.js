807c6b125e75874c9a094e7eb7ed5320dc50acb4
MuhitShimanto <muhit.shimanto02@gmail.com>
2025-08-26 22:24:15 +0600

    starter files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {},
+    screens: {
+        xs: 0,
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        '2xl': '1400px'
+    },
+  },
+}
