08957ebbca70cff2acf38a576dc00bf59099e637
MuhitShimanto <muhit.shimanto02@gmail.com>
2025-08-27 00:52:23 +0600

    Added Functionality of History Clear Button

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ let heartCounter = parseInt(document.getElementById("heart-count").innerText);
 let coinCounter = parseInt(document.getElementById("coin-count").innerText);
 let copyCounter = parseInt(document.getElementById("copy-count").innerText);
 
-let callHistoryArr = [];
-
 // Functions
 document
   .getElementById("card-container")
@@ -17,11 +15,11 @@ document
       const number = card.querySelector("h3").innerText;
       if (coinCounter >= 20) {
         alert(`Calling ${contact} ${number}...`);
-        coinCounter-=20;
+        coinCounter -= 20;
         document.getElementById("coin-count").innerText = coinCounter;
 
         const date = new Date().toLocaleTimeString();
-        const div = document.createElement('div');
+        const div = document.createElement("div");
         div.innerHTML = `
         <div class="flex justify-between bg-[#FAFAFA] p-[16px] rounded-xl">
             <div>
@@ -34,10 +32,10 @@ document
             </div>
             <div class="hind-madurai-regular text-[18px]">${date}</div>
           </div>
-        `
-        document.getElementById("history-card-container").appendChild(div); 
+        `;
+        document.getElementById("history-card-container").appendChild(div);
       } else {
-        alert("You do not have enough coins to make a call.")
+        alert("You do not have enough coins to make a call.");
       }
     }
     // Copy Btn
@@ -55,3 +53,7 @@ document
       document.getElementById("heart-count").innerText = heartCounter;
     }
   });
+
+  document.getElementById("history-clear-btn").addEventListener('click', function(){
+    document.getElementById("history-card-container").innerHTML="";
+  })
